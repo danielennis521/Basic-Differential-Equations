c7e95c0d32a1eeae0c282cc16d5829e7bfd738ff
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2023-06-04 22:59:17 -0500

    Update n_body_simulation.py

--- n_body_simulation.py
+++ n_body_simulation.py
@@ -3,11 +3,14 @@ import numpy as np
 from tkinter import *
 from tkinter import ttk
 
+dt = 0.01
+G = 6.67e-2
+#G = 6.67e-11
 
 def newtons_law(p1, p2):
     r = np.sqrt((p2.pos[0] - p1.pos[0])**2 + (p2.pos[1] - p1.pos[1])**2)
     f = ((p2.pos[0] - p1.pos[0])/r, (p2.pos[1] - p1.pos[1])/r)
-    F = 6.67e-11 * p1.mass * p2.mass / r**2
+    F = G * p1.mass * p2.mass / r**2
     return f[0]*F, f[1]*F
 
 
@@ -28,15 +31,15 @@ class nbody_sim:    # object that takes an array of points and uses them to simu
         self.points = points
         self.time = 0.0
         self.n = len(points)
-        self.forces = np.zeros(self.n, 2) 
+        self.forces = np.zeros((self.n, 2)) 
 
         self.gravity()
         temp = []
         for i in range(self.n):
-            x = self.points[i].pos[0] + 0.001*self.points[i].vel[0] + 0.5*self.forces[i][0]*0.00001/self.points[i].mass
-            y = self.points[i].pos[1] + 0.001*self.points[i].vel[1] + 0.5*self.forces[i][1]*0.00001/self.points[i].mass
-            vx = self.points[i].vel[0] + 0.001*self.forces[i][0]/self.points[i].mass
-            vy = self.points[i].vel[1] + 0.001*self.forces[i][1]/self.points[i].mass
+            x = self.points[i].pos[0] + dt*self.points[i].vel[0] + 0.5*self.forces[i][0]*dt**2/self.points[i].mass
+            y = self.points[i].pos[1] + dt*self.points[i].vel[1] + 0.5*self.forces[i][1]*dt**2/self.points[i].mass
+            vx = self.points[i].vel[0] + dt*self.forces[i][0]/self.points[i].mass
+            vy = self.points[i].vel[1] + dt*self.forces[i][1]/self.points[i].mass
 
             p = point([x, y], [vx, vy], self.points[i].mass)
             temp.append(p)
@@ -50,13 +53,13 @@ class nbody_sim:    # object that takes an array of points and uses them to simu
             f[0] = 0.0
             f[1] = 0.0
 
-        for i in range(self.n):
+        for i in range(self.n//2):
             for j in range(i+1, self.n):
-                f = newtons_law(self.points[i], self.points[j])
-                self.forces[i][0] += f[0]
-                self.forces[i][1] += f[1]
-                self.forces[j][0] -= f[0]
-                self.forces[j][1] -= f[1]
+                new_f = newtons_law(self.points[i], self.points[j])
+                self.forces[i][0] += new_f[0]
+                self.forces[i][1] += new_f[1]
+                self.forces[j][0] -= new_f[0]
+                self.forces[j][1] -= new_f[1]
 
 
 
@@ -64,14 +67,28 @@ class nbody_sim:    # object that takes an array of points and uses them to simu
         self.gravity()
 
         for i in range(self.n):
-            x = 2.0*self.points[i].pos[0] - self.prev[i].pos[0] + self.forces[i][0]*0.00001/self.points[i].mass
-            y = 2.0*self.points[i].pos[1] - self.prev[i].pos[1] + self.forces[i][1]*0.00001/self.points[i].mass
+            x = 2.0*self.points[i].pos[0] - self.prev[i].pos[0] + self.forces[i][0]*dt**2/self.points[i].mass
+            y = 2.0*self.points[i].pos[1] - self.prev[i].pos[1] + self.forces[i][1]*dt**2/self.points[i].mass
             self.prev[i].pos = self.points[i].pos
             self.points[i].pos = [x, y]
 
     
     
 # build the window
-root = Tk()
-title = Label(root, text='The N-body problem! (in 2-D)')
+# root = Tk()
+# title = Label(root, text='The N-body problem! (in 2-D)')
+frame = 12
+p1 = point([0.0, 5.0], [20.0, -5.0], 1)
+p2 = point([0.0, 0.0], [0.0, 0.0], 100000)
+sim = nbody_sim([p1, p2])
 
+
+for i in range(1000):
+    plt.xlim(-frame, frame)
+    plt.ylim(-frame, frame)
+    plt.plot(sim.points[0].pos[0], sim.points[0].pos[1], 'ro', color='blue', markersize=4)
+    plt.plot(sim.points[1].pos[0], sim.points[1].pos[1], 'ro', color='red', markersize=10)
+    plt.pause(0.001)
+    plt.cla()
+
+    sim.update_system()
